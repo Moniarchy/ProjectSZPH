18d047b167b60e052c9c14f58291e2c00e87f5c0
James D Stewart Jr <jamstewjr@gmail.com>
2016-12-24 13:31:37 -0500

    Add testing dependencies and create tests for App.js.

--- tests/src/components/App.test.js
+++ tests/src/components/App.test.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../../../src/components/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
+import renderer from 'react-test-renderer'
 
-it( 'renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+import App from '../../../src/components/App'
+
+describe( 'App component', () => {
+  const appWrapper = shallow( <App /> )
+
+  it( 'can be snapshot', () => {
+    const appSnapshot = renderer.create( <App /> ).toJSON()
+    expect( appSnapshot ).toMatchSnapshot()
+  })
+
+  it( 'renders without crashing', () => {
+    const appDiv = appWrapper.find( '.app' )
+    expect( appDiv.type() ).toEqual( 'div' )
+  })
+
+})
