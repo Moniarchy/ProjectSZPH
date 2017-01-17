e52d3bddb7dfc23008ad40379e91c521f98cc844
Monica Williams <monicaestellaw@gmail.com>
2017-01-17 11:32:14 -0800

    WIP: created basic console component that will house the text for the game

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from '../../public/assets/logo.svg';
+import ShipConsole from './ShipConsole'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="app-header">
-          <img src={logo} className="app-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="app-introduction">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ShipConsole />
       </div>
     );
   }
---
+++ src/components/ShipConsole.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ShipConsole extends Component {
+  render() {
+    return (
+      <div className="shipConsole">
+        <h1>test text</h1>
+      </div>
+    );
+  }
+}
